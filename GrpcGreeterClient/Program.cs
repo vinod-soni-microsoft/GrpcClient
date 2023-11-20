ed47f19e9fd017cd84ff23d80f95a23330dcb352
Vinod Soni <vinod.soni@microsoft.com>
2023-11-20 16:03:40 -0500

    Add project files.

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,16 @@
+﻿#region snippet2
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+#region snippet
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://grpcgreeter.azurewebsites.net");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "Vinod Soni calling... " });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+#endregion
+#endregion
